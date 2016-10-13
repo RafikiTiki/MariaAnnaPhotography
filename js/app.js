f339908057347814a8f279266f5b1a658d6d115f
RafikiTiki <wow1926@gmail.com>
2016-10-13 22:50:17 +0200

    added about section and handled menu events

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
 $(document).ready(function(){
+    var menuBurger = $(".menu__burger");
+
+    $("body").on("mouseup touchend ", function(e) {
+        var bool1 = menuBurger.hasClass("open");
+        var bool2 = !menuBurger.next().is(e.target);                                        // menu is not a target of an event
+        var bool3 = menuBurger.next().has(e.target).length === 0;                           // nor menu items
+        var bool4 = !(menuBurger.is(e.target)) && (menuBurger.has(e.target).length === 0);  // nor burger itself
+        if (bool1 && bool4 && bool2 && bool3 && bool4) {
+            menuBurger.removeClass('open');
+            menuBurger.next().slideUp();
+        }
+    });
+
 	$('.menu__burger').on("click", function(){
-		$(this).toggleClass('open');
-        $(this).next().slideToggle();
+		menuBurger.toggleClass('open');
+        menuBurger.next().slideToggle();
 	});
 });
