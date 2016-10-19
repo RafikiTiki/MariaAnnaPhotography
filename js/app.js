25886c1dec424fd3777c3428ea29322642f4db9a
RafikiTiki <wow1926@gmail.com>
2016-10-19 22:25:21 +0200

    added video and social sections

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     // it's the best solution that came to my mind to eliminate #header resize on mobile when browser toolbar disappears on scroll
     var height = $(window).height();
-    if(height < 560) {
+    if (height < 560) {
         $("#home").height(560);
     } else {
         $("#home").height(height);
@@ -12,11 +12,11 @@ $(document).ready(function(){
     var menuBurger = $(".menu__burger");
 
     $("body").on("click", function(e) {
-        var bool1 = menuBurger.hasClass("open");
-        var bool2 = !menuBurger.next().is(e.target);                                        // menu is not a target of an event
-        var bool3 = menuBurger.next().has(e.target).length === 0;                           // nor menu items
-        var bool4 = !(menuBurger.is(e.target)) && (menuBurger.has(e.target).length === 0);  // nor burger itself
-        if (bool1 && bool4 && bool2 && bool3 && bool4) {
+        var bool1 = menuBurger.hasClass("open"),
+            bool2 = !menuBurger.next().is(e.target),                                        // menu is not a target of an event
+            bool3 = menuBurger.next().has(e.target).length === 0,                           // nor menu items
+            bool4 = !(menuBurger.is(e.target)) && (menuBurger.has(e.target).length === 0);  // nor burger itself
+        if (bool1 && bool4 && bool2 && bool3) {
             menuBurger.removeClass('open');
             menuBurger.next().slideUp();
         }
@@ -37,19 +37,22 @@ $(document).ready(function(){
 
     // smooth scroll
     $(function() {
-        $('a[href*="#"]:not([href="#"])').click(function() {
+        $('a[href*="#"]').click(function() {
+            // block all empty links
+            if ($(this).attr("href") === "#") {
+                return false;
+            }
             if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
                 var target = $(this.hash);
                 target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
                 if (target.length) {
                     // this fixes smooth scroll when #nav menu is fixed
-                    var stickyMenuModifier;
-                    (target.attr("id") === "home" || target.attr("id") === "nav") ? stickyMenuModifier = 0 : stickyMenuModifier = menuHeight;
-                    // close mobile menu on scroll
-                    // console.log(target.attr("id"));
-                    // console.log($(target) == $("#home"));
-                    // console.log(target == $("#nav"));
-                    console.log(stickyMenuModifier);
+                    var stickyMenuModifier = (target.attr("id") === "home" || target.attr("id") === "nav") ? 0 : menuHeight;
+                    // this fixes smooth scroll on mobile when #nav menu is not fixed yet
+                    if (menuBurger.is(":visible") && !menuContainer.hasClass("nav--fixed") && stickyMenuModifier) {
+                        stickyMenuModifier += menuContainer.find(".menu__list").height();
+                    }
+                    // close menu on mobile
                     menuBurger.removeClass('open');
                     menuBurger.next().slideUp();
                     $('html, body').animate({
@@ -62,16 +65,18 @@ $(document).ready(function(){
     });
 
     // events that handle making menu fixed
-    var menuContainer = $("#nav");
-    var scrollValueAfterWhichMakeFixed = menuContainer.offset().top;
-    var html = $("html");
-    var menuHeight = menuContainer.height();
-    var headingAfterMenu = $(".about__title");
-    var HeadingMarginMin = headingAfterMenu.css("margin-top");
-    var headingMarginMax = parseInt(headingAfterMenu.css("margin-top")) + menuHeight + "px";
+    var menuContainer = $("#nav"),
+        scrollValueAfterWhichMakeFixed = menuContainer.offset().top,
+        html = $("html"),
+        body = $("body"),
+        menuHeight = menuContainer.height(),
+        headingAfterMenu = $(".about__title"),
+        HeadingMarginMin = headingAfterMenu.css("margin-top"),
+        headingMarginMax = parseInt(headingAfterMenu.css("margin-top")) + menuHeight + "px";
     // fixing and unfixing menu based on present scroll value
     $(window).on("scroll", function() {
-        if (html.scrollTop() >= scrollValueAfterWhichMakeFixed) {
+        // checking .scrollTop() on both html and body for compatibility with MS Edge
+        if ((html.scrollTop() || body.scrollTop()) >= scrollValueAfterWhichMakeFixed) {
             menuContainer.addClass("nav--fixed");
             headingAfterMenu.css("margin-top", headingMarginMax);
         } else {
